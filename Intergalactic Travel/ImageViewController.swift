393736c77d2bfb7d2d5ec425f4a0da58dff543ed
student1 <student1@jh-mbair-158-19.d214.org>
2015-11-06 09:48:57 -0600

    create new view controller and create new segues

---
+++ Intergalactic Travel/ImageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewController.swift
+//  Intergalactic Travel
+//
+//  Created by student1 on 11/6/15.
+//  Copyright © 2015 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
